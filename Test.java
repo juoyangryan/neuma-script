aa780890d6f80722c60812ba3e9f401c4a59c2b9
asteriajimin <52091175+asteriajimin@users.noreply.github.com>
2019-09-30 22:20:14 -0400

    Experiment and Test

---
+++ Test.java
@@ -0,0 +1,8 @@
+public class Test {
+	public static void main(String[] args) throws InterruptedException {
+		Experiment run1 = new Experiment();
+		run1.normalTrial();
+		run1.kineTrial();
+		run1.visualTrial();
+	}
+}
